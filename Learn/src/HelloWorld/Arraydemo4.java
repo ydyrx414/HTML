b90d1b666cad922182c464b771a11eb228fb5817
Bo Yu <ydyrx414@163.com>
2023-02-11 20:08:37 +0800

    demo

--- Learn/src/HelloWorld/Arraydemo4.java
+++ Learn/src/HelloWorld/Arraydemo4.java
@@ -2,6 +2,9 @@ package HelloWorld;
 
 public class Arraydemo4 {
     public static void main(String[] args) {
-
+        playgame();
+    }
+    public static void playgame(){
+        System.out.println("game");
     }
 }
---
+++ Learn/src/HelloWorld/Methoddemo.java
@@ -0,0 +1,12 @@
+package HelloWorld;
+
+public class Methoddemo {
+    public static void main(String[] args) {
+        getsum(10,20);
+    }
+
+    public static void getsum(int num,int num1) {
+        int result = num + num1;
+        System.out.println(result);
+    }
+}
